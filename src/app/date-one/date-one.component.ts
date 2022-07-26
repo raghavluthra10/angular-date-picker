a9e1e70f52682ef0202ba346b45c8962459f3426
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2022-07-26 21:36:56 +0530

    added disableDateRange in kendo-datepicker

--- src/app/date-one/date-one.component.ts
+++ src/app/date-one/date-one.component.ts
@@ -6,12 +6,36 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./date-one.component.css"],
 })
 export class DateOneComponent implements OnInit {
-  value: Date = new Date();
+  format = "MM/dd/yyyy HH:mm";
+  value: Date = new Date(2022, 7, 26, 21, 30);
+
+  min: Date = new Date();
+  max: Date = new Date(2022, 7, 26, 21, 10);
+  disableStartTime: string = "";
+  disableEndTime: string = "";
+  // tempDate: any
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // here we set the date as per our demands.
+    // for the sake of example, lets say 28th july to 30th july
+    // this.min = new Date(2022, 7, 28, 17)
+    console.log(this.value);
+    console.log(
+      "current: ",
+      this.value.getHours(),
+      ":",
+      this.value.getMinutes()
+    );
+  }
 
-  disabledDates = (date: Date): boolean => {
-    return date.getDate() % 2 === 0;
-  };
+  disabledDates: Date[] = [
+    new Date(2022, 7, 21, 21, 30),
+    new Date(2022, 7, 22, 21, 30),
+    new Date(2022, 7, 23, 21, 30),
+    new Date(2022, 7, 30, 21, 30),
+    // new Date(2022, 7, 26),
+    // new Date(2022, 7, 26),
+    // new Date(2022, 7, 26),
+  ];
 }
